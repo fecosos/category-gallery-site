680659ade595b01c5c931f1d049d704a07b6e7da
Ruy Mallo <ruymallo@gmail.com>
2019-07-03 23:38:00 -0300

    Add normalize to index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import 'normalize.css'
 import App from './Components/App';
 import * as serviceWorker from './serviceWorker';
 
