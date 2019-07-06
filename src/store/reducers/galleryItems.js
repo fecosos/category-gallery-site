f040f75dd56131ad58f6063843591939fe31868e
Ruy Mallo <ruymallo@gmail.com>
2019-07-06 17:43:34 -0300

    Create galleryItems reducer

---
+++ src/store/reducers/galleryItems.js
@@ -0,0 +1,7 @@
+import { GALLERY_ITEMS_INITIAL_STATE } from './galleryItemsInitialState';
+
+const initialState = GALLERY_ITEMS_INITIAL_STATE;
+
+export default function galleryItems(state = initialState, action = {}) {
+  return state;
+}
