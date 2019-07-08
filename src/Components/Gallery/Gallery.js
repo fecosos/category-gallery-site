62625d82200f95daedc1bde89904b89f8043807c
Ruy Mallo <ruymallo@gmail.com>
2019-07-08 03:41:11 -0300

    add hide and visible to GalleryItem's depending on the selected category

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import noop from 'lodash/noop';
 import './Gallery.scss';
 
-function GalleryItem({ item, onClick}) {
-  const { id, title, url, year } = item;
+
+const GalleryItem = ({ item, onClick, selectedCategory }) => {
+  const { id, title, url, year, category } = item;
+  const isHidden = selectedCategory && selectedCategory !== category;
+
+  const style = { opacity: isHidden ?  '0' : '1'};
+  
+  const clickHandler = () => isHidden ? noop : onClick;
+
   return (
-    <figure className="gallery-image-wrapper" id={id} onClick={onClick}>
+    <figure className="gallery-image-wrapper" id={id} onClick={clickHandler} style={style} >
       <img className="gallery-image-item" src={url} alt={title} />
       <figcaption>
         <p>{title}</p>
@@ -15,11 +22,12 @@ function GalleryItem({ item, onClick}) {
   );
 };
 
-function Gallery({ items, setGalleryById }) {
+function Gallery({ items, setGalleryById, selectedCategory }) {
   return (
     <div className="Gallery gallery-items">
       {items.map(item => (
         <GalleryItem
+          selectedCategory={selectedCategory}
           key={item.id}
           onClick={() => setGalleryById(item.id)}
           item={item} />
--- src/Components/Gallery/index.js
+++ src/Components/Gallery/index.js
@@ -2,9 +2,11 @@ import Gallery from './Gallery'
 import { connect } from 'react-redux';
 import { setGalleryById } from '../../store/actions/gallery';
 import { getGalleryIntros } from '../../store/selectors/gallery';
+import { getSelectedCategory } from '../../store/selectors/category';
 
 const mapStateToProps = state => ({
-  items: getGalleryIntros(state)
+  items: getGalleryIntros(state),
+  selectedCategory: getSelectedCategory(state)
 });
 
 const mapDispatchToProps = {
