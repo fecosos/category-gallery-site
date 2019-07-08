2a748e10940e1b7e002ed71d4f1fe0f8f6916654
Ruy Mallo <ruymallo@gmail.com>
2019-07-08 03:40:32 -0300

    Add selectedCategory and category to Nav

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,33 +1,31 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
-const NavItem = ({ title, openCategory }) => {
-  const renderWhenOpen = component => openCategory === title && component;
+import {
+  CATEGORY_CONCESIONARIAS,
+  CATEGORY_VIVIENDAS,
+  CATEGORY_BARES } from '../../store/constants';
+
+const NavItem = ({ title, selectedCategory, category, setCategory }) => {
+  const renderWhenOpen = component => selectedCategory === category && component;
 
   return (
     <li>
       {renderWhenOpen(<button>x</button>)}
-      <button>{title}</button>
+      <button onClick={() => setCategory(category)} >{title}</button>
       {renderWhenOpen(<button>Ver lista</button>)}
     </li>
   );
 }
 
-export function Navigation({ openCategory }) {
+export default function Navigation({ selectedCategory, setCategory }) {
   return (
     <nav>
       <ul>
-        <NavItem title="concesionarias" openCategory={openCategory} />
-        <NavItem title="viviendas" openCategory={openCategory} />
-        <NavItem title="bares" openCategory={openCategory} />
-        <NavItem title="el estudio" openCategory={openCategory} />
+        <NavItem setCategory={setCategory} category={CATEGORY_CONCESIONARIAS} title="concesionarias" selectedCategory={selectedCategory} />
+        <NavItem setCategory={setCategory} category={CATEGORY_VIVIENDAS} title="viviendas" selectedCategory={selectedCategory} />
+        <NavItem setCategory={setCategory} category={CATEGORY_BARES} title="bares" selectedCategory={selectedCategory} />
+        <NavItem setCategory={setCategory} category={null} title="el estudio" selectedCategory={selectedCategory} />
       </ul>
     </nav>
   );
 }
-
-const mapStateToProps = state => ({
-  openCategory: "viviendas"
-});
-
-export default connect(mapStateToProps)(Navigation);
--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -1,1 +1,15 @@
-export { default } from './Navigation';
+import Navigation from './Navigation';
+import { connect } from 'react-redux';
+import { setCategory } from '../../store/actions/category';
+import { getSelectedCategory } from '../../store/selectors/category';
+
+
+const mapStateToProps = state => ({
+  selectedCategory: getSelectedCategory(state)
+});
+
+const mapDispatchToProps = {
+  setCategory
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navigation);
