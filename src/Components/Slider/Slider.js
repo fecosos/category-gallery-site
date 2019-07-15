4b257eb8f7a5b25485a49d4c9259b30c2c9ac4eb
Ruy Mallo <ruymallo@gmail.com>
2019-07-15 13:28:43 -0300

    pass Slider width to its children

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import './Slider.scss';
 
-export default function Slider({ columnWidth, children, isOpen, closeSlider }) {
-  const left = isOpen ? `calc(50% - ${columnWidth}px)` : '100%';
+export default function Slider({ columnWidth, children, isOpen, closeSlider, width }) {
+  const sliderRef = useRef(null);
+  const left = isOpen ? `calc(50% - ${columnWidth}px)` : '200%';
+
+  const childrenWithWidth = React.Children.map(children, child => {
+    return React.cloneElement(child, { width });
+  });
   
   return (
-    <section className="Slider" style={{ left }} >
-      <button onClick={closeSlider} >&gt;</button>
-      {children}
+    <section ref={sliderRef} className="Slider" style={{ left }} >
+      {
+        isOpen ? 
+        <div className="content" >
+          <button className="close-button" onClick={closeSlider} >&gt;</button>
+          {childrenWithWidth}
+        </div>
+        : null
+      }
     </section>
   );
 }
