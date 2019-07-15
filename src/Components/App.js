1862bdbdb03fbbbac4bedc2c3e0a63f36dba4b79
Ruy Mallo <ruymallo@gmail.com>
2019-07-15 13:28:55 -0300

    add SwipeGalleryManager to App

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,22 +6,44 @@ import Gallery from './Gallery';
 import Navigation from './Navigation';
 import Slider from './Slider';
 
+import SwipeGalleryManager from './SwipeGalleryManager';
+
 import './App.scss'
 
+
+const Logo = () => (
+  <div className="logo-wrapper">
+    <button>
+      <img className="logo-img" src='https://dummyimage.com/75x75.jpg?text=R' alt='Logo' />
+    </button>
+  </div>
+);
+
+const InstagramAnchor = () => (
+  <div className="instagram-wrapper">
+    <a href="https://instagram.com" rel="noopener noreferrer" target="_blank" >
+      @instagram
+    </a>
+  </div>
+);
 
 function App({testState, galleryItems}) {
   const mainEl = useRef(null);
   const [columnWidth, setColumnWidth] = useState(0);
+  const [mainWidth, setMainWidth] = useState(0);
 
   const getColumnWidthFromRef = ref => ref.current.clientWidth / 3;
   const setColumnWidthFromRef = pipe(getColumnWidthFromRef, setColumnWidth);
   const resizeHandler = useCallback(() => {
     setColumnWidth(mainEl);
+    setMainWidth(mainEl.current.clientWidth);
   }, [])
-
+  
   useEffect(() => {
     setColumnWidthFromRef(mainEl);
+    setMainWidth(mainEl.current.clientWidth);
     window.addEventListener('resize', resizeHandler);
+    
 
     return () => window.removeEventListener('resize', resizeHandler);
   }, [resizeHandler, columnWidth, setColumnWidthFromRef]);
@@ -29,21 +51,15 @@ function App({testState, galleryItems}) {
   return (
     <div className="App" >
       <aside style={{ width: columnWidth}}>
-        <div className="logo-wrapper">
-          <button>
-            <img className="logo-img" src='https://dummyimage.com/75x75.jpg?text=R' alt='Logo' />
-          </button>
-        </div>
+        <Logo />
         <Navigation />
-        <div className="instagram-wrapper">
-          <a href="https://instagram.com" rel="noopener noreferrer" target="_blank" >
-            @instagram
-          </a>
-        </div>
+        <InstagramAnchor />
       </aside>
       <main ref={mainEl} >
         <Gallery />
-        <Slider columnWidth={columnWidth} />
+        <Slider columnWidth={columnWidth} width={mainWidth} >
+          <SwipeGalleryManager />
+        </Slider>
       </main>
     </div>
   );
