b9f4c5aec7c8be7745eb29139b2a41c820a16cf7
Ruy Mallo <ruymallo@gmail.com>
2019-07-16 00:47:34 -0300

    Create selector getSelectedSliderDescription

--- src/store/selectors/slider.js
+++ src/store/selectors/slider.js
@@ -1,3 +1,21 @@
 import get from 'lodash/fp/get';
+import map from 'lodash/map';
 
 export const getIsSliderOpen = state => !!get('slider.isOpen')(state);
+
+export const getSelectedSliderDescription = state => {
+  const selectedId = get('slider.id')(state);
+
+  const itemYear = get(`galleryItems.${selectedId}.year`)(state);
+  
+  const description = map(
+    get(`galleryItems.${selectedId}.description`)(state),
+    (item, index) => {
+      if(index === 0) {
+        return `${item} [${itemYear}]`;
+      }
+      return item;
+  });
+
+  return description || [];
+}
