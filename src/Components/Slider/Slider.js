640ed6ef911a80c3f6f43c21045b04205a09c7de
Ruy Mallo <ruymallo@gmail.com>
2019-07-16 00:48:07 -0300

    Add opacity transition to Slider

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -4,6 +4,7 @@ import './Slider.scss';
 export default function Slider({ columnWidth, children, isOpen, closeSlider, width }) {
   const sliderRef = useRef(null);
   const left = isOpen ? `calc(50% - ${columnWidth}px)` : '200%';
+  const contentOpacityClassName = isOpen ? `opacity-1` : 'opacity-0';
 
   const childrenWithWidth = React.Children.map(children, child => {
     return React.cloneElement(child, { width });
@@ -11,14 +12,10 @@ export default function Slider({ columnWidth, children, isOpen, closeSlider, wid
   
   return (
     <section ref={sliderRef} className="Slider" style={{ left }} >
-      {
-        isOpen ? 
-        <div className="content" >
-          <button className="close-button" onClick={closeSlider} >&gt;</button>
-          {childrenWithWidth}
-        </div>
-        : null
-      }
+      <div className={`content ${contentOpacityClassName}`} id="slider-container" >
+        <button className="close-button" onClick={closeSlider} >&gt;</button>
+        {childrenWithWidth}
+      </div>
     </section>
   );
 }
